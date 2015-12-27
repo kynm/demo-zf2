cfbd88f5f763004f56a9ee43e6394b90a3f57728
kynm <nguyen.manh.ky@framgia.com>
2015-12-27 22:36:35 +0700

    demo create module zf2

---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,94 @@
+<?php
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Album\Model\Album;
+use Album\Form\AlbumForm;
+
+class AlbumController extends AbstractActionController
+{
+    protected $albumTable;
+    public function indexAction()
+    {
+        $albums = $this->getAlbumTable()->fetchAll();
+        $views['hello'] = 'hello world!';
+        $views['albums'] = $albums;
+        return $views;
+    }
+
+    public function addAction()
+    {
+        $form = new AlbumForm();
+        $form->get('submit')->setValue('Add');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $album = new Album();
+            $form->setInputFilter($album->getInputFilter());
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $album->exchangeArray($form->getData());
+                $this->getAlbumTable()->saveAlbum($album);
+                return $this->redirect()->toRoute('album');
+            }
+        }
+        $views['form'] = $form;
+        return $views;
+    }
+
+    public function editAction()
+    {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+            return $this->redirect()->toRoute('album', array(
+                'action' => 'add'
+            ));
+        }
+        try {
+            $album = $this->getAlbumTable()->getAlbum($id);
+        } catch (\Exception $ex) {
+            return $this->redirect()->toRoute('album', array(
+                'action' => 'index'
+            ));
+        }
+
+        $form  = new AlbumForm();
+        $form->bind($album);
+        $form->get('submit')->setAttribute('value', 'Edit');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $form->setInputFilter($album->getInputFilter());
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $this->getAlbumTable()->saveAlbum($album);
+                 return $this->redirect()->toRoute('album');
+            }
+        }
+        $views['id'] = $id;
+        $views['form'] = $form;
+        return $views;
+    }
+
+    public function deleteAction()
+    {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+            return $this->redirect()->toRoute('album');
+        }
+        $this->getAlbumTable()->deleteAlbum($id);
+        return $this->redirect()->toRoute('album');
+    }
+
+     public function getAlbumTable()
+     {
+         if (!$this->albumTable) {
+             $sm = $this->getServiceLocator();
+             $this->albumTable = $sm->get('Album\Model\AlbumTable');
+         }
+         return $this->albumTable;
+     }
+}
